d7dcd92c173d8ccf563f4809ec53a30a21152210
Maksim_Shramko <maxramko@gmail.com>
2025-10-21 20:48:09 +0300

    Compleating main

--- shramko.maksim/F0/main.cpp
+++ shramko.maksim/F0/main.cpp
@@ -1,2 +1,110 @@
-int main()
-{}
+#include <iostream>
+#include <string>
+#include <fstream>
+#include <cctype>
+#include "dictionary_manager.hpp"
+#include "commands.hpp"
+#include <FwdList.hpp>
+#include <HashTable/hash_table.hpp>
+
+shramko::FwdList< std::string > splitString(const std::string& str)
+{
+  shramko::FwdList< std::string > tokens;
+  std::string currentToken;
+  size_t start = 0;
+  size_t end = str.length();
+
+  while (start < end && std::isspace(static_cast< unsigned char >(str[start])))
+  {
+    start++;
+  }
+
+  for (size_t i = start; i < end; ++i)
+  {
+    if (std::isspace(static_cast< unsigned char >(str[i])))
+    {
+      if (!currentToken.empty())
+      {
+        tokens.push_back(currentToken);
+        currentToken.clear();
+      }
+    }
+    else
+    {
+      currentToken += str[i];
+    }
+  }
+
+  if (!currentToken.empty())
+  {
+    tokens.push_back(currentToken);
+  }
+  return tokens;
+}
+
+int main(int argc, char* argv[])
+{
+  DictionaryManager dm;
+  shramko::hash_table< unsigned char > commandMap = createCommandMap();
+
+  if (argc == 2)
+  {
+    std::string file = argv[1];
+    if (!dm.createDict("main"))
+    {
+      std::cout << "DICT EXISTS\n";
+    }
+    else if (!dm.loadFromFile("main", file))
+    {
+      std::cout << "FILE NOT FOUND\n";
+    }
+  }
+
+  std::string line;
+  while (std::getline(std::cin, line))
+  {
+    if (line.empty())
+    {
+      continue;
+    }
+
+    shramko::FwdList< std::string > tokens = splitString(line);
+    if (tokens.empty())
+    {
+      continue;
+    }
+
+    auto it = tokens.begin();
+    std::string commandName = *it;
+    ++it;
+
+    shramko::FwdList< std::string > args;
+    for (; it != tokens.end(); ++it)
+    {
+      args.push_back(*it);
+    }
+
+    auto cmdIt = commandMap.find(commandName);
+    if (cmdIt != commandMap.end())
+    {
+      try
+      {
+        cmdIt->second(args, dm, std::cout);
+      }
+      catch (const std::exception& e)
+      {
+        std::cout << "ERROR Unhandled exception: " << e.what() << "\n";
+      }
+      catch (...)
+      {
+        std::cout << "ERROR Unknown unhandled exception occurred.\n";
+      }
+    }
+    else
+    {
+      std::cout << "INVALID COMMAND\n";
+    }
+  }
+
+  return 0;
+}
